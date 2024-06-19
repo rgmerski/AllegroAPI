d442efd501f15859e7acdc4697d48145bb4ee7c2
Robert <rgmerski@gmail.com>
2024-06-19 20:01:00 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AllegroAPI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
